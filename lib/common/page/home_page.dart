36c34be6da2432461f0410619594d58a8ed7776d
yunrui <yunrui@staff.weibo.com>
2020-01-08 10:51:38 +0800

    [Update] #0

--- lib/common/page/home_page.dart
+++ lib/common/page/home_page.dart
@@ -6,45 +6,55 @@ import 'package:ledger/common/page/feed_page.dart';
 
 import 'mine_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomeState();
+}
+
+class _HomeState extends State<HomePage> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-
-    // todo
-    getPageStream().transform<BottomNavigationBarItem>(
-        (StreamTransformer<Map<String, dynamic>, BottomNavigationBarItem>
-            transformer) {
-      return null;
-    });
+    /*getPageStream().transform<BottomNavigationBarItem>(StreamTransformer<
+        Map<String, dynamic>,
+        BottomNavigationBarItem>.fromHandlers(handleData: (data, sink) {
+      sink.add(BottomNavigationBarItem(
+        icon: Icon(data["icon"]),
+        title: Text(data["name"]),
+      ));
+    }));*/
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("账本"),
       ),
-      body: null,
-      bottomNavigationBar: BottomNavigationBar(items: null
-          /*        .transform<BottomNavigationBarItem>(
-              StreamTransformer < Map < String, dynamic>>,
-              BottomNavigationBarItem > streamTransformer){
-      return null;
-      }*/
-
-          ),
+      body: getPageList()[_currentIndex]["page"],
+      bottomNavigationBar: BottomNavigationBar(
+        items: null,
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: (index) {
+          setState(() {
+            this._currentIndex = index;
+          });
+        },
+      ),
     );
   }
 
-  Stream<Map<String, dynamic>> getPageStream() {
-    StreamController<Map<String, dynamic>> controller = new StreamController();
-    controller.sink.add({
-      "name": "账本",
-      "icon": Icons.book,
-      "page": FeedPage,
-    });
-    controller.sink.add({
-      "name": "我",
-      "icon": Icons.person,
-      "page": MinePage,
-    });
-    return controller.stream;
+  List<Map<String, dynamic>> getPageList() {
+    return [
+      {
+        "name": "账本",
+        "icon": Icons.book,
+        "page": FeedPage(),
+      },
+      {
+        "name": "我",
+        "icon": Icons.person,
+        "page": MinePage(),
+      }
+    ];
   }
 }

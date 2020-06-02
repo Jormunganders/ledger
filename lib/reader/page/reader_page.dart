cd3439a2cf0dfd880ad593011422ee75930a4936
qiaoyunrui <qiaoyunrui@kuaishou.com>
2020-06-02 21:23:52 +0800

    [Add] 网络测试代码

--- lib/reader/page/reader_page.dart
+++ lib/reader/page/reader_page.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:ledger/common/net/net.dart';
 
 class ReaderPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("神庙"),
-      ),
-      body: Center(
-        child: Text("这一页还没有想好要做什么，别急"),
-      ),
-    );
+    return Theme(
+        data: READER_THEME_DATA,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("御书房"),
+          ),
+          body: Center(
+            child: RaisedButton(
+              child: Text("Get"),
+              onPressed: () {
+                print("Start Get");
+                getDio()
+                    .get("http://www.baidu.com")
+                    .then((value) => print);
+              },
+            ),
+          ),
+        ));
   }
 }
+
+// ignore: non_constant_identifier_names
+final READER_THEME_DATA = new ThemeData(
+  primaryColor: Colors.teal,
+  accentColor: Colors.redAccent,
+);
